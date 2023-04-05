9b7e12fd43e7e827349604b489ed5721045d55bd
Kleber Neto <kleberneto29@gmail.com>
2023-04-05 16:38:46 -0300

    alteraçao

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+ // Crie uma função para verificar a senha:
 
+ function checkPassword() {
+  // Obtenha os valores de usuário e senha do formulário
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-function closeMenu() {
-  const link =  'href="/sobre.html"'
-  return link
-
+  // Verifique se a senha é correta
+  if (username === "Coisinha" && password === "1131518") {
+    // Se a senha estiver correta, redirecione para a página protegida
+    window.location.href = "pagina_protegida.html";
+  } else {
+    // Se a senha estiver incorreta, exiba uma mensagem de erro
+    alert("Nome de usuário ou senha incorretos.");
+  }
 }
 
-closeMenu()
+ // Adicione um ouvinte de eventos para o formulário de login:
+
+var form = document.getElementById("login-form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Impede que o formulário seja enviado
+  checkPassword(); // Verifique a senha
+});
